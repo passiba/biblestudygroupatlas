830d4c7b380722df42135ed37493ac2bffde9bd7
pasishemeikka@hotmail.com <pasishemeikka@hotmail.com@37ef7822-87e1-11dd-b2bb-736fb4d8a922>
2008-09-22 19:37:02 +0000

    used perhaps in different context, saved for now

--- biblestudygroupatlas/biblestudywebservice/src/main/java/fi/passiba/biblestudy/crosswaybible/webservice/client/BibleVerseClient.java
+++ biblestudygroupatlas/biblestudywebservice/src/main/java/fi/passiba/biblestudy/crosswaybible/webservice/client/BibleVerseClient.java
@@ -5,11 +5,10 @@
 package fi.passiba.biblestudy.crosswaybible.webservice.client;
 
 import fi.passiba.biblestudy.crosswaybible.webservice.client.domain.RequestDetails;
-import fi.passiba.biblestudy.crosswaybible.webservice.client.jaxb.request.EvalutateBibleVerseReguest;
+import fi.passiba.biblestudy.crosswaybible.webservice.client.jaxb.request.doPassageQueryRequest;
 import fi.passiba.biblestudy.crosswaybible.webservice.client.jaxb.response.CrosswayBible;
 import fi.passiba.biblestudy.crosswaybible.webservice.client.jaxb.response.DoPassageQueryResponse;
 import java.io.IOException;
-import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 /**
@@ -26,7 +25,7 @@ public class BibleVerseClient  extends WebServiceGatewaySupport{
             throws IOException {
 
 
-        EvalutateBibleVerseReguest request = new EvalutateBibleVerseReguest();
+        doPassageQueryRequest request = new doPassageQueryRequest();
         request.setKey(requestDetails.getKey());
         request.setPassage(requestDetails.getPassage());
         request.setOptions(requestDetails.getOptions());
