16c0b36563e373a0d7ca21f99f202e33395ab118
pasishemeikka@hotmail.com <pasishemeikka@hotmail.com@37ef7822-87e1-11dd-b2bb-736fb4d8a922>
2008-10-17 16:19:01 +0000

    modifying the front end for viewing bible verses information

--- biblestudygroupatlas/biblestudywar/src/main/java/fi/passiba/groups/ui/pages/biblesession/ChapterPanel.java
+++ biblestudygroupatlas/biblestudywar/src/main/java/fi/passiba/groups/ui/pages/biblesession/ChapterPanel.java
@@ -1,11 +1,27 @@
 package fi.passiba.groups.ui.pages.biblesession;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import fi.passiba.biblestudy.BibleStudySession;
+import fi.passiba.groups.ui.model.DomainModelIteratorAdaptor;
+import fi.passiba.groups.ui.model.HashcodeEnabledCompoundPropertyModel;
+import fi.passiba.groups.ui.pages.search.ListPersons;
+import fi.passiba.groups.ui.pages.user.EditPersonContact;
+import fi.passiba.groups.ui.pages.user.ViewPersonContact;
 import fi.passiba.services.biblestudy.persistance.Chapter;
+import fi.passiba.services.biblestudy.persistance.Verse;
+import fi.passiba.services.persistance.Person;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.RefreshingView;
+import org.apache.wicket.markup.repeater.ReuseIfModelsEqualStrategy;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.PropertyModel;
 
@@ -27,7 +43,9 @@ public class ChapterPanel extends AbstractDataPanel {
 
         add(new Label("chapterTitle", new PropertyModel(getModel(), "chapterTitle")));
         add(new Label("chapterNum", new PropertyModel(getModel(), "chapterNum")));
-        
+        RefreshingView verses=populateBibleVerses(chapterid);
+        verses.setItemReuseStrategy(ReuseIfModelsEqualStrategy.getInstance());
+        add(verses);
       
     }
      
@@ -37,5 +55,43 @@ public class ChapterPanel extends AbstractDataPanel {
   {
       return BibleStudySession.get().isAuthenticated();
   }
+    
+  
+  private RefreshingView populateBibleVerses(final long chapterid)
+  {
+            RefreshingView bibleVerses = new RefreshingView("bibleVerses") {
+
+            List<Verse> result = new ArrayList<Verse>(0);
+
+            @Override
+            protected Iterator getItemModels() {
+                
+                  result = bibleTranslationDataRetrievalService.findVersesByChapterId(chapterid);
+                   
+                
+                return new DomainModelIteratorAdaptor<Verse>(result.iterator()) {
+
+                    @Override
+                    protected IModel model(final Object object) {
+                        
+                      return new HashcodeEnabledCompoundPropertyModel((Verse) object);
+                    }
+                };
+            }
+            @Override
+            protected void populateItem(Item item) {
+
+                item.add(new Label("verseNum",
+                        new PropertyModel(item.getModel(), "verseNum")));
+
+
+                item.add(new Label("verseText",
+                        new PropertyModel(item.getModel(), "verseText")));
+
+                
+            }
+        };
+        return   bibleVerses;
+    }
 
 }
