b87a06448a4408a8b95df7afbc27726aece1dbb2
pasishemeikka@hotmail.com <pasishemeikka@hotmail.com@37ef7822-87e1-11dd-b2bb-736fb4d8a922>
2008-10-24 19:05:45 +0000

    adding modification of editing verse text

--- trunk/biblestudygroupatlas/biblestudywar/src/main/java/fi/passiba/groups/ui/pages/biblesession/NewVerseForm.java
+++ trunk/biblestudygroupatlas/biblestudywar/src/main/java/fi/passiba/groups/ui/pages/biblesession/NewVerseForm.java
@@ -5,45 +5,63 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 
 import fi.passiba.services.biblestudy.persistance.Verse;
-import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.PropertyModel;
+import org.wicketstuff.dojo.markup.html.inlineeditbox.DojoInlineEditBox;
 
 public final class NewVerseForm extends AbstractDataPanel {
 
+    private Verse verse= new Verse();
+    private  Component dojoInlineEditBox;
     public NewVerseForm(String id, Chapter chapter) {
 
         super(id);
 
-        IModel model = new CompoundPropertyModel(new LoadableDetachableModel() {
-            @Override
-            public Object load() {
-                return new Verse();
-            }
-        });
+        verse.setVerseText("Jakeen teksti");
+
+        IModel model = new CompoundPropertyModel(verse);
         setModel(model);
         init(chapter);
     }
 
     private void init(Chapter chapter) {
+        
+         final VerseForm verseForm=new VerseForm("form", getModel(), chapter);
+         dojoInlineEditBox = new DojoInlineEditBox("verseText", new PropertyModel(getModel(), "verseText")) {
 
-        add(new VerseForm("form", getModel(), chapter));
+                protected void onSave(AjaxRequestTarget target) {
+                   // verse = (Verse) verseForm.getModelObject();
+                    verse.setVerseText(dojoInlineEditBox.getModelObjectAsString());
+                    target.addComponent(dojoInlineEditBox);
+                }
+         };
+         dojoInlineEditBox.setVisible(true);
+		 dojoInlineEditBox.setOutputMarkupPlaceholderTag(true);
+         verseForm.add(dojoInlineEditBox);
+         add(verseForm);
 
     }
 
     private final class VerseForm extends Form {
 
+
+        
         public VerseForm(String id, IModel m, Chapter chapter) {
             super(id, m);
-            add(new CancelButton("cancelButton",chapter.getId()));
+            add(new CancelButton("cancelButton", chapter.getId()));
             add(new SaveButton("saveButton", chapter));
             TextField verseNum = new TextField("verseNum", new PropertyModel(getModel(), "verseNum"));
+            //verseNum.setRequired(true);
             add(verseNum);
-            TextArea verseText = new TextArea("verseText", new PropertyModel(getModel(), "verseText"));
-            add(verseText);
+            // TextArea verseText = new TextArea("verseText", new PropertyModel(getModel(), "verseText"));
+
+           
+            //verseText.setRequired(true);
         }
         private final class SaveButton extends Button {
 
@@ -55,7 +73,8 @@ public final class NewVerseForm extends AbstractDataPanel {
 
             @Override
             public void onSubmit() {
-                Verse verse = (Verse) getForm().getModelObject();
+                verse = (Verse) getForm().getModelObject();
+                verse.setVerseText(NewVerseForm.this.dojoInlineEditBox.getModelObjectAsString());
                 verse.setChapter(chapter);
                 bibleTranslationDataRetrievalService.addVerse(verse);
                 NewVerseForm.this.replaceWith(new ChapterPanel("bibleSessionpanel",chapter.getId()).setOutputMarkupId(true));
@@ -76,4 +95,6 @@ public final class NewVerseForm extends AbstractDataPanel {
             }
         }
     }
+
+
 }
