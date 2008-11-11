655794502ef36ae9210cd81a79b97dd0520f6adf
pasishemeikka@hotmail.com <pasishemeikka@hotmail.com@37ef7822-87e1-11dd-b2bb-736fb4d8a922>
2008-11-11 20:01:05 +0000

    initial implementation JSword Book Installer factory

---
+++ biblestudygroupatlas/biblestudywar/src/main/java/fi/passiba/services/bibledata/SiteEditor.java
@@ -0,0 +1,72 @@
+/**
+ * Distribution License:
+ * BibleDesktop is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 2 as published by
+ * the Free Software Foundation. This program is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * The License is available on the internet at:
+ *       http://www.gnu.org/copyleft/gpl.html
+ * or by writing to:
+ *      Free Software Foundation, Inc.
+ *      59 Temple Place - Suite 330
+ *      Boston, MA 02111-1307, USA
+ *
+ * Copyright: 2005
+ *     The copyright to this program is held by it's authors.
+ *
+ * ID: $Id: SiteEditor.java 776 2005-09-10 09:07:17Z dmsmith $
+ */
+package fi.passiba.services.bibledata;
+
+import java.util.List;
+import java.util.Map;
+import org.crosswire.jsword.book.install.Installer;
+
+/**
+ * A SiteEditor allows for the editing of a site.
+ * 
+ * @see gnu.gpl.License
+ * @author DM Smith [dmsmith555 at yahoo dot com]
+ */
+public interface SiteEditor
+{
+    /**
+     * Establish the installer for this SiteEditor.
+     * @param newInstaller
+     */
+    public void setInstaller(Installer newInstaller);
+
+    /**
+     * Get the installer associated with this SiteEditor
+     * @return the installer
+     */
+    public Installer getInstaller();
+
+    
+
+    /**
+     * Save the state of this SiteEditor.
+     */
+    public void save();
+
+    /**
+     * Set the state of this SiteEditor to the last save.
+     */
+    public void reset();
+
+    /**
+     * Returns the configured installers
+     *
+     * @return Map installers
+     */
+    public Map getInstallers();
+    
+    
+  
+
+
+
+}
