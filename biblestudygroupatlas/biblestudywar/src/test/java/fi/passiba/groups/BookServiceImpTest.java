c1c085b0d4550eac6e2126ac6abeb3a6e5df25fb
pasishemeikka@hotmail.com <pasishemeikka@hotmail.com@37ef7822-87e1-11dd-b2bb-736fb4d8a922>
2008-12-01 14:03:23 +0000

    updating the Test Cases to use AbstractTransactionalJUnit4SpringContext abstract class

--- biblestudygroupatlas/biblestudywar/src/test/java/fi/passiba/groups/BookServiceImpTest.java
+++ biblestudygroupatlas/biblestudywar/src/test/java/fi/passiba/groups/BookServiceImpTest.java
@@ -1,17 +1,26 @@
 package fi.passiba.groups;
 
-import fi.passiba.AbstractDependencyInjectionSpringContextTest;
+import fi.passiba.AbstractTransactionalJUnit4SpringContext;
 import fi.passiba.services.biblestudy.dao.IBookDAO;
 import fi.passiba.services.biblestudy.persistance.Book;
-
+import static org.junit.Assert.*;
 import java.util.List;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-public final class BookServiceImpTest extends AbstractDependencyInjectionSpringContextTest {
-  
+ @Transactional(propagation = Propagation.SUPPORTS,readOnly=true)
+public final class BookServiceImpTest extends AbstractTransactionalJUnit4SpringContext {
+
+     @Autowired
+     private IBookDAO bookDAO;
+    
+     @Test
      public void testFindingBookByBookSectionId() throws Exception {
-        IBookDAO bookDAO = (IBookDAO) applicationContext.getBean("BookDAO");
+       
         List<Book> books = bookDAO.findBooksByBooksectionId(1);
-        //assertEquals(26, books.size());
+        //assertEquals(66, books.size());
     }
      /*public void testFindingBookByDataSourceId() throws Exception {
         IBookDAO bookDAO = (IBookDAO) applicationContext.getBean("BookDAO");
