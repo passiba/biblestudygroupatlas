79d8963890c5e0fb1d9c8e77798caaf802ecf1ba
pasishemeikka@hotmail.com <pasishemeikka@hotmail.com@37ef7822-87e1-11dd-b2bb-736fb4d8a922>
2008-12-15 09:19:42 +0000

    group panel implementation added

--- trunk/biblestudygroupatlas/biblestudywar/src/main/java/fi/passiba/groups/ui/pages/search/ListPersonsPanel.java
+++ trunk/biblestudygroupatlas/biblestudywar/src/main/java/fi/passiba/groups/ui/pages/search/ListPersonsPanel.java
@@ -29,7 +29,7 @@ import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 
-public class ListPersonsPanel extends Panel {
+public final class ListPersonsPanel extends Panel {
     
 
     @SpringBean
